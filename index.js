55e69da0c3a0a3059121e02b413947c631987eac
Michelle Antelo <michelle@Michelles-MacBook-Pro.local>
2022-08-11 14:14:29 -0400

    break save

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 let image = document.getElementById("image");
 let topText = document.getElementById("topText");
-let bottomText = document.getElementById("bottomText");
+let bottomText = document.getElementsByTagName("bottomText");
+let meme = document.getElementById("meme");
+let generateButton = document.getElementById('generate');
 
+function generateMeme(){
+    
+}
+
+generateButton.addEventListener('click', function(){
+    alert("BUTTON IS WORKING!!");
+});
