80b7185cba92d23cd1370cc2565be1c67aa2a26d
Michelle Antelo <michelle@Michelles-MacBook-Pro.local>
2022-08-11 15:21:51 -0400

    linked js

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ let image = document.getElementById("image");
 let topText = document.getElementById("topText");
 let bottomText = document.getElementsByTagName("bottomText");
 let meme = document.getElementById("meme");
-let generateButton = document.getElementById('generate');
+let generateButton = document.getElementsByClassName('block');
 
-function generateMeme(){
-    
-}
+function generateMeme(){}
 
 generateButton.addEventListener('click', function(){
     alert("BUTTON IS WORKING!!");
